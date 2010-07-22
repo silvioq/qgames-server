704df7507996b3e19626bcc4c2376d124061269a
Silvio <silvioq@gmail.com>
2010-07-22 06:55:25 -0300

    first commit

---
+++ test/test-main.c
@@ -0,0 +1,19 @@
+/*
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * Silvio Quadri 2010.
+ * */
+
+#include  <stdlib.h>
+#include  <stdio.h>
+#include  <qgames.h>
+#include  <qgames_analyzer.h>
+
+int main(int argc, char** argv){
+
+    Tipojuego* tj = tipojuego_new( "Hola" );
+
+    exit( EXIT_SUCCESS );
+}
+
